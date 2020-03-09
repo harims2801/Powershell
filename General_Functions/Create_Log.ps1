427ea65a37a3d2af3eef1e66fecd43d4723be223
harims2801 <39764403+harims2801@users.noreply.github.com>
2020-03-09 13:21:44 -0400

    Update Create_Log.ps1

--- General_Functions/Create_Log.ps1
+++ General_Functions/Create_Log.ps1
@@ -8,3 +8,18 @@ function Write-Log($text) {
 }
 
 Write-Log "Append text to log file"
+
+
+
+Function Clear-logs{
+	$files = dir "*failover_cluster_bot.log" | where { $($(get-date) - $($_.CreationTime)).Days -ge $Log_duration}
+	
+	foreach ($file in $files){
+		try{
+			remove-item $file
+		}catch{
+			write-Log "Error Deleting log file $file"
+		}
+		write-Log "log file - $file has been deleted"
+	}
+}
